d0ec65cf3e244cbd781a7a63171382f8eab1c85f
Your Name <you@example.com>
2022-07-20 22:55:05 +0900

    ex6 filter add

--- src/html/es6/method.js
+++ src/html/es6/method.js
@@ -1,7 +1,17 @@
-export const map= (f, iterator) => {
+export const map = (f, iterator) => {
   let res = [];
   for (const a of iterator) {
     res.push(f(a));
+  }
+  return res;
+};
+
+export const filter = (f, iterator) => {
+  let res = [];
+  for (const a of iterator) {
+    if (f(a)) {
+      res.push(a);
+    }
   }
   return res;
 }
