74446c4ee6f326fea416757b802ddf703d643fb4
kimym123 <kwa124@naver.com>
2022-07-21 23:39:44 +0900

    reduce add

--- src/html/es6/reduce.js
+++ src/html/es6/reduce.js
@@ -11,3 +11,4 @@ console.log(`기존 방법을 통한 reduce 형태의 방법 : `, total); // 15
 const add = (a, b) => a + b;
 console.log(`만들었던 reduce 함수를 이용한 방법 : `,  reduce(add, 0, [1, 2, 3, 4, 5]));
 console.log(`만들었던 reduce 함수를 이용한 방법 & acc 인자를 생략한 경우 : `,  reduce(add, [1, 2, 3, 4, 5]));
+asd
